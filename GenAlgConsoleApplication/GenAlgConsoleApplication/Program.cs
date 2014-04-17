cef08c0abf00c2d6d4e621c07c9a37d77faba316
maxnagibator <managibator@gmail.com>
2014-04-17 15:46:05 +0700

    generate population

---
+++ GenAlgConsoleApplication/GenAlgConsoleApplication/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace GenAlgConsoleApplication
+{
+    internal class Program
+    {
+        private const int PERSON_COUNT = 10;
+        private const int GEN_COUNT = 8;
+        private static int[,] _population;
+
+        private static void Main(string[] args)
+        {
+            //пусть вся область у нас будет числа от 0 до 255
+            GenerateOdeyaloPopulation();
+            PopulationShow("Odeyalo");
+            GenerateDrobovikPopulation();
+            PopulationShow("Drobovik");
+            GenerateFocusPopulation();
+            PopulationShow("Focus");
+        }
+
+        private static void GenerateOdeyaloPopulation()
+        {
+            //некоторая заданная область будет у нас от 15 до (15 + PERSON_COUNT)
+            _population = new int[PERSON_COUNT, GEN_COUNT];
+            const int startValue = 15;
+            for (int i = 0; i < PERSON_COUNT; i++)
+            {
+                var currentValue = startValue + i;
+                for (int j = 0; j < GEN_COUNT; j++)
+                {
+                    _population[i, j] = currentValue%2;
+                    currentValue = currentValue/2;
+                }
+            }
+        }
+
+        private static void GenerateDrobovikPopulation()
+        {
+            //берём случайные от всего решения
+            _population = new int[PERSON_COUNT, GEN_COUNT];
+            var rnd = new Random();
+            for (int i = 0; i < PERSON_COUNT; i++)
+            {
+                var currentValue = rnd.Next(0, 255);
+                for (int j = 0; j < GEN_COUNT; j++)
+                {
+                    _population[i, j] = currentValue%2;
+                    currentValue = currentValue/2;
+                }
+            }
+        }
+
+        private static void GenerateFocusPopulation()
+        {
+            //берём случайные в выбранном диапозоне от 50 до 100
+            _population = new int[PERSON_COUNT, GEN_COUNT];
+            var rnd = new Random();
+            for (int i = 0; i < PERSON_COUNT; i++)
+            {
+                var currentValue = rnd.Next(50, 100);
+                for (int j = 0; j < GEN_COUNT; j++)
+                {
+                    _population[i, j] = currentValue%2;
+                    currentValue = currentValue/2;
+                }
+            }
+        }
+
+        private static void PopulationShow(string str)
+        {
+            Console.WriteLine(str);
+            for (int i = 0; i < PERSON_COUNT; i++)
+            {
+                Console.Write(i + ": ");
+                for (int j = 0; j < GEN_COUNT; j++)
+                {
+                    Console.Write(_population[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ GenAlgConsoleApplication/GenAlgConsoleApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("GenAlgConsoleApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenAlgConsoleApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("becd61d2-757c-4f05-9c2b-c8b307d0009a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
