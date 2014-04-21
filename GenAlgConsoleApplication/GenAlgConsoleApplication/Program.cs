9a1f687e778446031c1f6478d0cd616b2ac0c2eb
maxnagibator <managibator@gmail.com>
2014-04-21 14:27:07 +0700

    кроссинговер одноточечный

--- GenAlgConsoleApplication/GenAlgConsoleApplication/Program.cs
+++ GenAlgConsoleApplication/GenAlgConsoleApplication/Program.cs
@@ -5,75 +5,77 @@ namespace GenAlgConsoleApplication
     internal class Program
     {
         private const int PERSON_COUNT = 10;
-        private const int GEN_COUNT = 2;
+        private const int GEN_COUNT = 8;
         private static int[,] _population;
+        private static int[,] _populationAfterKrossingover;
+        private static int[] _tochkiRazriva;
+        //•	Одноточечного оператора
+        //•	Двухточечного
+        //•	Трёхточечного
+        //•	Универсального
+        //•	Упорядочивающего одно- и двухточечный операторы кроссинговера
+        //•	Частично-соответствующего одно- и двухточечному операторам кроссинговера
+        //•	Циклического оператора
+        //•	«Жадного» оператора
+        //•	Одно-, двух- и трёх точечного операторов на основе принципа «золотого сечения» и чисел Фибоначчи.
 
         private static void Main(string[] args)
         {
-            //пусть вся область первого гена у нас будет числа от 0 до 255
-            //значение второго гена у нас будет от 50 до 100
-            GenerateOdeyaloPopulation();
-            PopulationShow("Odeyalo");
+            //пусть вся область всех генов у нас будет числа от 0 до 255
+            //ВИД ГЕНА: числовой!
             GenerateDrobovikPopulation();
-            PopulationShow("Drobovik");
-            GenerateFocusPopulation()   ;
-            PopulationShow("Focus");
+            PopulationsShow("Начальная популяция");
+            KrossingoverOdnotochechniy();
+            PopulationsShowAfterKrossingover("После кроссинговера Одноточечного");
         }
 
-        /* 1. Выбрать диапазон десятичных значений (напр. - от 15 до 25)
-            2. Создать цикл. Длина цикла - количество этих значений (особей).
-               В каждой итерации цикла закодировать это десятичное число, представляющее особь,
-               в двоичном формате. Т.о., получаем двоичный вектор, являющийся геномом особи.
-         * 
-         *  УТОЧНИТЬ: 
-         *  Делается выборка непрерывного диапазона или можно из разных частей?
-         *  Можно брать выборку не всех значений?
-         */
-        private static void GenerateOdeyaloPopulation()
+        private static void KrossingoverOdnotochechniy()
         {
-            //некоторая заданная область будет у нас от 15 до (15 + PERSON_COUNT)
-            _population = new int[PERSON_COUNT,GEN_COUNT];
-            const int startValue = 15;
-            const int start2GenValue = 65; //значение второго гена от 65 до 75
-            for (int i = 0; i < PERSON_COUNT; i++)
+            //Перед началом работы одноточечного оператора кроссинговера определяется так называемая точка ОК, 
+            //или разрезающая точка ОК, которая обычно определяется случайно.
+            //Эта точка определяет место в двух хромосомах, где они должны быть «разрезаны».
+            _populationAfterKrossingover = new int[PERSON_COUNT, GEN_COUNT];
+            _tochkiRazriva = new int[PERSON_COUNT];
+            //Console.Write("Точки разрыва: ");
+            var rnd = new Random();
+            for (int i = 0; i < PERSON_COUNT; i += 2)
             {
-                // особь из 2-х генов. Если их должно быть больше, соответственно, изменить колич. процедур.
-                // в данный момент получаем десятичные значения. Если нужно кодировать (1-й вариант), 
-                // - преобразовать в двоичный формат.
-                _population[i, 0] = startValue + i;
-                _population[i, 1] = start2GenValue + i;
+                var tochkaRazriva = rnd.Next(0, GEN_COUNT + 1);
+                _tochkiRazriva[i/2] = tochkaRazriva;
+                //Console.Write("{0,1} ", tochkaRazriva);
+                for (int j = 0; j < GEN_COUNT; j++)
+                {
+                    if (tochkaRazriva > j)
+                    {
+                        _populationAfterKrossingover[i + 1, j] = _population[i, j];
+                        _populationAfterKrossingover[i, j] = _population[i + 1, j];
+                    }
+                    else
+                    {
+                        _populationAfterKrossingover[i, j] = _population[i, j];
+                        _populationAfterKrossingover[i + 1, j] = _population[i + 1, j];
+                    }
+                }
             }
+            Console.WriteLine();
         }
 
         private static void GenerateDrobovikPopulation()
         {
-            //берём случайные от всего решения
+            //берём случайные от всего решения и каждому гену присваивем случайное число
             _population = new int[PERSON_COUNT, GEN_COUNT];
             var rnd = new Random();
             for (int i = 0; i < PERSON_COUNT; i++)
             {
-                var currentValue = rnd.Next(0, 255);
-                var current2GenValue = rnd.Next(50, 100);
-                _population[i, 0] = currentValue;
-                _population[i, 1] = current2GenValue;
+                for (int j = 0; j < GEN_COUNT; j++)
+                {
+                    var currentValue = rnd.Next(0, 255);
+                    _population[i, j] = currentValue;
+                }
             }
         }
 
-        private static void GenerateFocusPopulation()
-        {
-            //берём случайные в выбранном диапозоне от 50 до 100
-            _population = new int[PERSON_COUNT, GEN_COUNT];
-            var rnd = new Random();
-            for (int i = 0; i < PERSON_COUNT; i++)
-            {
-                var currentValue = rnd.Next(50, 100);
-                var current2GenValue = rnd.Next(70, 93);
-                _population[i, 0] = currentValue;
-                _population[i, 1] = current2GenValue;
-            }
-        }
-
-        private static void PopulationShow(string str)
+        private static void PopulationsShow(string str)
         {
             Console.WriteLine(str);
             for (int i = 0; i < PERSON_COUNT; i++)
@@ -81,9 +83,38 @@ namespace GenAlgConsoleApplication
                 Console.Write(i + ": ");
                 for (int j = 0; j < GEN_COUNT; j++)
                 {
-                    Console.Write(_population[i, j]+" ");
+                    Console.Write("{0,4} ", _population[i, j]); // {0,4} - это для отступов, чтобы красиво отображалось
                 }
                 Console.WriteLine();
+            }
+        }
+
+        private static void PopulationsShowAfterKrossingover(string str)
+        {
+            //показываю для каждой пары популяций до и после значения и точку разрыва
+            Console.WriteLine(str);
+            for (int i = 0; i < PERSON_COUNT; i += 2)
+            {
+                Console.Write("ДО: \r\n" + i + ": ");
+                PopulationShow(i, _population);
+                Console.WriteLine();
+                Console.Write(i + 1 + ": ");
+                PopulationShow(i + 1, _population);
+                Console.WriteLine();
+                Console.Write("ПОСЛЕ: точка разрыва=" + _tochkiRazriva[i/2] + "\r\n" + i + ": ");
+                PopulationShow(i, _populationAfterKrossingover);
+                Console.WriteLine();
+                Console.Write(i + 1 + ": ");
+                PopulationShow(i + 1, _populationAfterKrossingover);
+                Console.WriteLine("\r\n______________________________");
+            }
+        }
+
+        private static void PopulationShow(int i, int[,] population)
+        {
+            for (int j = 0; j < GEN_COUNT; j++)
+            {
+                Console.Write("{0,4} ", population[i, j]);
             }
         }
     }
